bc61425fcfd422b0a78bae303371a2df62d55bc0
Prakash-Rajagopal <63907034+Prakash-Rajagopal@users.noreply.github.com>
2021-04-16 11:24:56 +0530

    Initial

---
+++ WindowsFormsApp12/WindowsFormsApp12/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        MySQLConnection con = new MySQLConnection();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loaddata();
+        }
+
+        private void loaddata()
+        {            
+            DataSet ds = new DataSet();
+            ds = con.LoadData("SELECT * FROM Position");
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != string.Empty && textBox2.Text != string.Empty) //check if all fields are filled
+            {
+                con.Executing("INSERT INTO Position (PositionId, Position ) VALUES ('" + textBox1.Text + "','" + textBox2.Text + "'); ");
+                loaddata();
+                MessageBox.Show("Record saved");
+            }
+            else
+            {
+                MessageBox.Show("Please fill all the fields", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp12/WindowsFormsApp12/MySQLConnection.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WindowsFormsApp12
+{
+   public class MySQLConnection
+    {
+         public SqlConnection connection;
+
+        public SqlConnection CreateConnection() //create mysql connection
+        {
+            string connectionString = "Data Source=DESKTOP-1D3IG54\\SQLEXPRESS;Initial Catalog=Craftsman;Integrated Security=True;";
+           connection = new SqlConnection(connectionString);
+            return connection;
+        }
+
+        private bool OpenConnection() //connect open
+        {
+            try
+            {
+                connection = CreateConnection();
+                connection.Open();
+                return true;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        private bool CloseConnection() //conne ction close
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Executing(string query) //execute sql query
+        {
+            try
+            {
+                if (OpenConnection())
+                {
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    cmd.ExecuteNonQuery();
+                    CloseConnection();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public DataSet LoadData(string Query) //get data using sql query
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                if (OpenConnection())
+                {
+
+                    SqlCommand cmd = new SqlCommand(Query, connection);
+                    cmd.CommandType = CommandType.Text;
+
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        sda.Fill(ds);
+                    }
+
+                    CloseConnection();
+                }
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+    }
+}
